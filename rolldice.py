12eaeb1fc9282d6cfc4713fb9eedc978a35f6511
Matheus Henri França <47190319+AlixHenri@users.noreply.github.com>
2023-02-28 13:50:01 -0300

    Dice drawings added
    --Now when i dice is rolled a drawing of one of the dice sides using characters will be printed in the terminal.

--- rolldice.py
+++ rolldice.py
@@ -1,17 +1,65 @@
 import random
 
 def roll_dice():
+
+    dice_drawing = {
+        1: (
+            "-------",
+            "|       |",
+            "|   o   |",
+            "|       |",
+            "-------",
+        ),
+        2: (
+            "---------",
+            "|o      |",
+            "|       |",
+            "|      o|",
+            "---------",
+        ),
+        3: (
+            "---------",
+            "|o      |",
+            "|   o   |",
+            "|      o|",
+            "---------",
+        ),
+        4: (
+            "---------",
+            "|o     o|",
+            "|       |",
+            "|o     o|",
+            "---------",
+        ),
+        5: (
+            "---------",
+            "|o     o|",
+            "|   o   |",
+            "|o     o|",
+            "---------",
+        ),
+        6: (
+            "---------",
+            "|o     o|",
+            "|o     o|",
+            "|o     o|",
+            "---------",
+        ),
+    }
+  
     roll = input("Do you want to roll the dice? (Yes/No): ").upper()
 
     if roll == "YES" or roll == "Y":
         dice1 = random.randint(1,6)
         print(f'First dice rolled: {dice1}')
+        print('\n'.join(dice_drawing[dice1]))
         roll_again = input('Do you want to roll another dice? (Yes/No): ').upper()
 
         if roll_again == "YES" or roll_again == 'Y':
             dice2 = random.randint(1,6)
             total = dice1 + dice2
             print(f"Second dice rolled: {dice2}")
+            print('\n'.join(dice_drawing[dice2]))
             print(f'Total: {total}')
         else:
             print(f'Total {dice1}')
